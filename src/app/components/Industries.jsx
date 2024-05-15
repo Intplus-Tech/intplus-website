5038d5d3a824d2ff1e6338b6b19cdef436023e24
jesutobiDev <jesutobi_dev@outlook.com>
2024-05-15 18:06:20 +0100

    changes

--- src/app/components/Industries.jsx
+++ src/app/components/Industries.jsx
@@ -1,21 +1,14 @@
 'use client'
-import React, {useState} from 'react';
-import Image from 'next/image'
-import cards from '../deck-data'
+import React from 'react';
+import Slider from './Slider'
+
+
 
 
 
 const Industries = () => {
-    const [currentIndex, setCurrentIndex] = useState(0);
 
-    const goToPrevious = () => {
-      setCurrentIndex((prevIndex) => (prevIndex === 0 ? cards.length - 1 : prevIndex - 1));
-    };
-  
-    const goToNext = () => {
-      setCurrentIndex((prevIndex) => (prevIndex === cards.length - 1 ? 0 : prevIndex + 1));
-    };
-  
+
   return (
     <div className='bg-[#D9E2FF] w-full min-h-[504px] lg:min-h-[512px] px-7 py-12 lg:p-20  flex flex-col lg:flex-row justify-between gap-20 relative'>
       <video autoPlay loop muted className='absolute inset-0 w-full h-full object-cover mix-blend-overlay'>
@@ -27,43 +20,8 @@ const Industries = () => {
         <p className='text-[#000000]'>As a versatile service provider, we cater to diverse industries, offering tailored solutions to meet specific needs. We ensure success across varied business landscapes with comprehensive and adaptable solutions.</p>
         <a href='#' className="bg-[#2D7EFF] w-fit py-3 px-6 rounded-full text-white">All Cases</a>
       </div>
-
-      {/* Deck */}
-      <div className='relative h-auto p-2 lg:w-[45%] transition-all duration-300 ease-in-out leading-[25px]'>
-<div className={`${cards[currentIndex].background} shadow-md  p-8 rounded-2xl flex flex-col justify-between gap-2 bottom-0 z-50 `}>
-          <div className='flex gap-4 items-base mb-5'>
-            <Image
-              className="flex self-end"
-              src={cards[currentIndex].icon}
-              alt=''
-              width={35}
-              height={35}
-            />
-            <h1 className="font-semibold text-2xl">{cards[currentIndex].industry}</h1>
-          </div>
-          <p className="text-sm mb-7 leading-[20px]">{cards[currentIndex].text}</p>
-
-          <div className='flex justify-between mt-2'>
-            <div onClick={goToPrevious}>
-              <Image
-                className={`rotate-180 cursor-pointer`}
-                src="/blackarrow.svg"
-                alt=''
-                width={25}
-                height={15}
-              />
-            </div>
-            <div onClick={goToNext}>
-              <Image
-                className={`cursor-pointer`}
-                src="/blackarrow.svg"
-                alt=''
-                width={25}
-                height={15}
-              />
-            </div>
-          </div>
-        </div>
+      <div className="w-full lg:w-[45%]  z-10 bg-green-400">
+        <Slider />
       </div>
     </div>
   )
--- src/app/deck-data.js
+++ src/app/deck-data.js
@@ -1,40 +1,34 @@
 const industriesData = [
     {
         icon : '/ico tech.svg',
-        industry: 'Technology',
-        text : 'We propel tech advancement through bespoke solutions, ensuring seamless integration and innovation for businesses. Our expertise drives digital transformation, providing customized strategies to navigate the evolving landscape of technology. We are committed to delivering excellence, empowering our clients to thrive in the ever-changing world of technology.',
+        industry: 'FinTech',
+        text : 'Accelerate innovation using the power of <span class="font-bold"> Int+</span> software development for banking and financial services. Advance your efficiency, customer experience, and business performance with our future-proof solutions',
         background : 'bg-[#F8FBFF]',
     },
     {
         icon : '/ico ecomm.svg',
-        industry: 'Ecommerce',
-        text : 'At PK Innovatives, we propel tech advancement through bespoke solutions, ensuring seamless integration and innovation for businesses. Our expertise drives digital transformation, providing customized strategies to navigate the evolving landscape of technology. We are committed to delivering excellence, empowering our clients to thrive in the ever-changing world of technology.',
+        industry: 'Ecommerce & Retail',
+        text : 'Create new opportunities for your business and for your customers with the help of specialized software. Transform the shopping experience with an engaging mobile app or improve business operations with a comprehensive enterprise software suite.',
         background : 'bg-[#E4EEFF]',
-    },
-    {
-        icon : '/ico hosp.svg',
-        industry: 'Hospital',
-        text : 'At PK Innovatives, we propel tech advancement through bespoke solutions, ensuring seamless integration and innovation for businesses. Our expertise drives digital transformation, providing customized strategies to navigate the evolving landscape of technology. We are committed to delivering excellence, empowering our clients to thrive in the ever-changing world of technology.',
-        background : 'bg-[#B8D2FD]',
-    },
-    {
-        icon : '/ico manu.svg',
-        industry: 'Manufacturing',
-        text : 'At PK Innovatives, we propel tech advancement through bespoke solutions, ensuring seamless integration and innovation for businesses. Our expertise drives digital transformation, providing customized strategies to navigate the evolving landscape of technology. We are committed to delivering excellence, empowering our clients to thrive in the ever-changing world of technology.',
-        background : 'bg-[#7FB0FF]',
-    },
-    {
-        icon : '/ico edu.svg',
-        industry: 'Education',
-        text : 'At PK Innovatives, we propel tech advancement through bespoke solutions, ensuring seamless integration and innovation for businesses. Our expertise drives digital transformation, providing customized strategies to navigate the evolving landscape of technology. We are committed to delivering excellence, empowering our clients to thrive in the ever-changing world of technology.',
-        background : 'bg-[#2D7EFF]',
     },
     {
         icon : '/ico ship.svg',
         industry: 'Logistics and Shipping',
-        text : 'At PK Innovatives, we propel tech advancement through bespoke solutions, ensuring seamless integration and innovation for businesses. Our expertise drives digital transformation, providing customized strategies to navigate the evolving landscape of technology. We are committed to delivering excellence, empowering our clients to thrive in the ever-changing world of technology.',
-        background : 'bg-[#0061FC]',
-    }
+        text : 'Logistics Software solutions with the latest tech give you an edge over competition across the board, to track your operations and predict malfunction. Get valuable insights into your operations.',
+        background : 'bg-[#B8D2FD]',
+    },
+    {
+        icon : '/ico travels.svg',
+        industry: 'Travels & Hospitality',
+        text : "Meet your customers' and partners’ needs with our end-to-end technology solutions for hospitality. From impactful mobile applications to comprehensive custom-made systems, leading brands rely on Int+ expertise and engineering experience.",
+        background : 'bg-[#7FB0FF]',
+    },
+    {
+        icon : '/ico real estate.svg',
+        industry: 'REAL ESTATE & PROPERTY',
+        text : ' Embolden your sales efforts through smarter software. Our team of experts has over nine years of experience in creating ERP and CRM solutions to help you manage sales leads, track reliable sales data, foster client engagement, and optimize your services for prospect and clients.',
+        background : 'bg-[#3A86FF]',
+    },
 ];
 
 export default industriesData;
