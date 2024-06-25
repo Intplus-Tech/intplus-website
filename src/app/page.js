0743505da7c49bdc94ead80fa6795371e84a2696
jesutobiDev <jesutobi_dev@outlook.com>
2024-06-25 16:54:42 +0100

    continuation

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import Header from './components/homepage/Header.jsx';
 // import ServiceSlider from './components/homepage/ServiceSlider.jsx';
-// import BuildingSection from './components/homepage/BuildingSection.jsx';
+import BuildingSection from './components/homepage/BuildingSection.jsx';
 import WorkApproach from './components/homepage/WorkApproach.jsx';
 // import DevelopmentProcess from './components/homepage/DevelopmentProcess.jsx';
 import CTA from './components/homepage/CTA.jsx';
@@ -13,7 +13,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between overflow-x-hidden relative">
       <Header />
       {/* <ServiceSlider/> */}
-      {/* <BuildingSection/> */}
+      <BuildingSection/>
       <WorkApproach/>
       {/* <DevelopmentProcess/> */}
       <CTA/>
